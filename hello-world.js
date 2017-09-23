d83fd2eb377418a8a4f76f6ab10a6babc4fcbac6
BillChandler <bchandler.cca0@gmail.com>
2017-09-23 05:53:35 -0400

    prints odd's increments

--- hello-world.js
+++ hello-world.js
@@ -4,7 +4,7 @@ var whoToGreet = "Suwanee";
 var howManyTime = 10;
 for (var i = 0; i <= 10; i++) {
 
-    if ( i%2 == 0){
+    if ( i%2 != 0){
 
         console.log(i + ": hello world " + whoToGreet);
     }
