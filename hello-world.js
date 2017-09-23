83a5f6449ebb97dd6215c8c5abf46bf80661dba8
BillChandler <bchandler.cca0@gmail.com>
2017-09-23 05:47:56 -0400

    1st hello-world commit

---
+++ hello-world.js
@@ -0,0 +1,8 @@
+// hello-world js
+// 
+var whoToGreet = "Suwanee";
+var howManyTime = 10;
+for (var i = 0; i < 10; i++) {
+    k = i + 1;  // setting a natural number to display in the string
+    console.log(k + ": hello world " + whoToGreet);
+}
