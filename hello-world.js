d347fdb17ce15f74bc97afd1b8ae9ad355a4285a
BillChandler <bchandler.cca0@gmail.com>
2017-09-23 05:51:35 -0400

    display evens only

--- hello-world.js
+++ hello-world.js
@@ -2,7 +2,11 @@
 // 
 var whoToGreet = "Suwanee";
 var howManyTime = 10;
-for (var i = 0; i < 10; i++) {
-    k = i + 1;  // setting a natural number to display in the string
-    console.log(k + ": hello world " + whoToGreet);
+for (var i = 0; i <= 10; i++) {
+
+    if ( i%2 == 0){
+
+        console.log(i + ": hello world " + whoToGreet);
+    }
+
 }
